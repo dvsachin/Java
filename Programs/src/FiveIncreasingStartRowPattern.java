ee8a09543c3e39b2f4f73e36a1d72de360f88421
dvsachin <dvsachin9@gmail.com>
2023-03-28 12:00:36 +0530

    day-4

---
+++ Programs/src/FiveIncreasingStartRowPattern.java
@@ -0,0 +1,25 @@
+public class FiveIncreasingStartRowPattern {
+    public static void main(String[] args) {
+        int i=1;int n=5;
+
+        while(i<=n){
+            int j=1;
+            while(j<=i){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
+
+/*
+
+ *
+ **
+ ***
+ ****
+ *****
+
+ */
