bbc65b2d8b85756c6c485f618c7631c4660f21e2
dvsachin <dvsachin9@gmail.com>
2023-04-10 12:26:38 +0530

    day-5 resolved error

--- Programs/src/PyramidStarPattern.java
+++ Programs/src/PyramidStarPattern.java
@@ -9,7 +9,7 @@ class PyramidStarPattern {
             int j=1;
             while(j<=n-i){
                 System.out.print(" ");
-                j++;1
+                j++;
             }
             int k=1;
             while(k<=2*i-1){
