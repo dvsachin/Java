627c8c2e6ceb4ebe0ed24b00bf6b6e2ca4d6be36
dvsachin <dvsachin9@gmail.com>
2023-04-28 20:02:49 +0530

    day-18

---
+++ Problems/src/geeksforgeeks/CheckIfArrayIsSorted.java
@@ -0,0 +1,61 @@
+package geeksforgeeks;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class CheckIfArrayIsSorted {
+    static boolean arraySortedOrNot(int[] arr, int n) {
+        // code here
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] < arr[i-1]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size  = sc.nextInt();
+        int[] arr = new int[size];
+
+        for(int i = 0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        if(arraySortedOrNot(arr,size))
+            System.out.println(1);
+        else
+            System.out.println(0);
+
+
+    }
+}
+
+/*
+Given an array arr[] of size N, check if it is sorted in non-decreasing order or not.
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {10, 20, 30, 40, 50}
+Output: 1
+Explanation: The given array is sorted.
+Example 2:
+
+Input:
+N = 6
+arr[] = {90, 80, 100, 70, 40, 30}
+Output: 0
+Explanation: The given array is not sorted.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function arraySortedOrNot() which takes the arr[] and N as input parameters and returns a boolean value (true if it is sorted otherwise false).
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 10^5
+1 ≤ Arr[i] ≤ 10^6
+ */
--- Problems/src/geeksforgeeks/MaximumAreaRectangle.java
+++ Problems/src/geeksforgeeks/MaximumAreaRectangle.java
@@ -1,2 +1,92 @@
-package geeksforgeeks;public class MaximumAreaRectangle {
+package geeksforgeeks;
+
+import java.util.Scanner;
+
+class Rectangle {
+    int length;
+    int breadth;
+
+    Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
 }
+
+public class MaximumAreaRectangle {
+    static int calculate_Area(Rectangle[] arr){
+
+        // Your code here
+        int[] areas = new int[arr.length];
+        int maxArea = 0;
+
+        for(int i=0; i< arr.length; i++) {
+            areas[i] = arr[i].length * arr[i].breadth;
+        }
+
+        for(int i=0; i< areas.length; i++) {
+            if(maxArea < areas[i]) maxArea = areas[i];
+        }
+
+        return maxArea;
+
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        Rectangle[] rectPair = new Rectangle[size];
+
+        for(int i = 0; i < size; i++) {
+            int length = scanner.nextInt();
+            int breadth = scanner.nextInt();
+            rectPair[i] = new Rectangle(length,breadth);
+
+        }
+
+        System.out.println(calculate_Area(rectPair));
+    }
+}
+
+/*
+Given N  pairs of rectangles, where each pair denotes the length L and breadth B of the ith rectangle. The task is to return the maximum area of the rectangle.
+
+Example 1:
+
+Input:
+N = 3
+rect[] = {{1,2},
+          {3,4},
+          {5,6}}
+Output:
+30
+
+Explanation:
+1. Total area of Rect. 1 = 1 * 2 = 2
+2. Total area of Rect. 2 = 3 * 4 = 12
+3. Total area of Rect. 3 = 5 * 6 = 30
+4. Out of all rectangles, Rectangle 3 has the maximum area.
+
+Example 2:
+
+Input:
+N = 2
+rect[] = {{12,3},
+          {40,5}}
+
+Output:
+200
+
+Explanation:
+1. Total area of Rect. 1 = 12 * 3 = 36
+2. Total area of Rect. 2 = 40 * 5 = 200.
+3. Out of all rectangles, Rectangle 2 has the maximum area.
+User Task: Your task is to complete the function calculate_Area() which returns maximum area. Use area() function in Rectangle class.
+
+Constraints:
+1 <= N <= 103
+1 <= L, B <= 104
+1 <= Area <= 108
+ */
