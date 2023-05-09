6fd8b1314a012f7c15044d095dc41496ecc5f457
dvsachin <dvsachin9@gmail.com>
2023-05-09 12:16:58 +0530

    day-27

--- Problems/src/geeksforgeeks/FindDistinctValues.java
+++ Problems/src/geeksforgeeks/FindDistinctValues.java
@@ -5,9 +5,40 @@ import java.util.Scanner;
 
 public class FindDistinctValues {
     public static int distinct(int M[][], int N) {
+        int[] elements = new int[N];
+        int count = 1, output = 0;
+        boolean flag = true, rowFlag = true;
+
+        elements[0] = M[0][0];
+        for(int i=1; i<N; i++) {
+            rowFlag = true;
+            int value = M[0][i];
+            for(int j=0; j<N; j++) {
+                if(value == elements[j]) rowFlag = false;
+            }
+            if(rowFlag) elements[i] = M[0][i];
+        }
+
+        for(int i=0; i<N; i++) {
+            for(int j=1; j<N; j++) {
+                for(int k=0; k<N; k++) {
+                    if(elements[i] == M[j][k]) {
+                        count++;
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) break;
+            }
+            if(count == N)
+                output++;
+
+            count = 1;
 
 
-        return  0;
+        }
+        return output;
+
     }
 
     public static void main(String[] args) {
@@ -17,6 +48,35 @@ public class FindDistinctValues {
 //12 1 14 3 16 14 2 1 3 35 14 1 14 3 11 14 25 3 2 1 1 18 3 21 14
 // output: 3
 
+//  4
+// 2 1 4 3 1 2 3 2 3 6 2 3 5 2 5 3
+// output: 2
+
+// 4
+// 1 2 1 2 2 2 2 1 2 1 2 2 2 1 1 1
+// output: 2
+
+// 18
+// 5 1 6 6 1 7 1 6 6 8 7 1 8 5 2 9 9 4
+// 9 4 7 8 1 4 6 2 6 9 5 4 5 7 4 1 3 4
+// 5 1 8 9 8 3 7 6 8 8 6 5 1 5 6 7 3 6
+// 8 7 7 4 6 1 5 8 7 9 6 8 1 2 8 6 1 7
+// 9 7 1 5 3 4 9 1 9 3 7 9 8 3 6 4 4 1
+// 4 9 6 8 8 3 6 6 2 3 3 2 7 2 6 7 4 6
+// 2 3 7 8 3 2 5 1 5 2 2 6 9 3 5 5 9 1
+// 5 5 7 6 5 7 5 2 8 8 7 2 5 6 2 9 4 4
+// 8 8 2 1 7 2 7 4 4 9 9 1 1 4 6 5 8 8
+// 2 3 1 7 2 5 6 4 1 5 1 2 6 8 7 5 9 5
+// 6 6 6 8 3 5 8 3 7 2 5 5 1 4 5 8 1 6
+// 1 4 7 1 8 7 9 4 6 6 9 3 8 5 6 5 1 8
+// 7 9 9 2 8 4 6 8 6 9 4 6 3 4 8 1 2 6
+// 5 1 1 8 5 7 1 1 9 4 5 1 3 1 7 2 2 5
+// 3 6 2 8 5 3 3 5 7 1 5 8 6 1 7 4 6 2
+// 8 7 9 8 1 3 6 1 3 3 2 2 5 3 5 4 1 7
+// 7 3 3 2 1 7 7 7 5 4 8 2 3 7 6 3 3 4
+// 5 8 5 5 8 4 6 1 4 2 5 5 8 9 5 1 1 6
+
+// output: 1 (8)
             int[][] arr = new int[size][size];
 
             for(int i =0; i<arr.length; i++) {
@@ -25,6 +85,7 @@ public class FindDistinctValues {
                 }
 
             }
+
             System.out.println(distinct(arr,size));
         }
 }
@@ -42,7 +103,7 @@ M = {{2, 1, 4, 3},
      {5, 2, 5, 3}}
 Output:
 2
-Explaination: Only 2 and 3 are common in all rows.
+Explanation: Only 2 and 3 are common in all rows.
 Example 2:
 
 Input:
--- Problems/src/geeksforgeeks/Test.java
+++ Problems/src/geeksforgeeks/Test.java
@@ -2,11 +2,18 @@ package geeksforgeeks;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr = new int[] {2,9,8,4,6,1,7,5,3,0};
-        for(int i = 0; i < arr.length; i++) {
-            arr[i/2] = arr[arr[arr[i/3]/3]/3];
-        }
-        for(int x : arr)
-        System.out.println(x);
+//        int[] arr = new int[] {2,9,8,4,6,1,7,5,3,0};
+//        for(int i = 0; i < arr.length; i++) {
+//            arr[i/2] = arr[arr[arr[i/3]/3]/3];
+//        }
+//        for(int x : arr)
+//        System.out.println(x);
+
+      //  System.out.println(new String(new char[] {'a','b','c'}));
+
+        int[] arr = {65,66,67,68};
+        String s = new String(arr,1,3);
+        System.out.println(s);
+
     }
 }
