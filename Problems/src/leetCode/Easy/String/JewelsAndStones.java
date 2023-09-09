a942df3dd2f4a9e7e359c6c4d20ce5a94b51016e
dvsachin <dvsachin9@gmail.com>
2023-09-09 14:40:23 +0530

    day-109 GFG-Leetcode

---
+++ Problems/src/leetCode/Easy/String/JewelsAndStones.java
@@ -0,0 +1,32 @@
+package leetCode.Easy.String;
+// https://leetcode.com/problems/jewels-and-stones/
+public class JewelsAndStones {
+    public int numJewelsInStones(String jewels, String stones) {
+        int jewelStone = 0;
+
+        for(int i=0; i<jewels.length(); i++) {
+            for(int j=0; j<stones.length(); j++) {
+                if(jewels.charAt(i) == stones.charAt(j)) jewelStone++;
+            }
+        }
+        return jewelStone;
+    }
+}
+
+/*
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+
+ */
