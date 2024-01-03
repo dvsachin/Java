3bb8258646af027c1ed969e524891364b9af4527
dvsachin <dvsachin9@gmail.com>
2024-01-03 20:18:41 +0530

    Day-3:2024 = POTD

--- Problems/src/Test.java
+++ Problems/src/Test.java
@@ -3,4 +3,21 @@ import java.util.Scanner;
 
 class Test {
 
+    static boolean checkPalindrome(String str) {
+        int start = 0, end = str.length()-1;
+
+        while(start < end){
+            if(str.charAt(start) != str.charAt(end))
+                return false;
+
+            start++;end--;
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        String no = "aabbaa";
+
+        System.out.println(checkPalindrome(no));
+    }
 }
---
+++ Problems/src/geeksforgeeks/Easy_Level/SmallestWindowContaining_0_1_and_2.java
@@ -0,0 +1,54 @@
+package geeksforgeeks.Easy_Level;
+
+public class SmallestWindowContaining_0_1_and_2 {
+    public int smallestSubstring(String S) {
+        // Code here
+        int zero=-1,one=-1,two=-1,res = 100001;
+
+        for(int i=0; i<S.length(); i++){
+            if(S.charAt(i)=='0') zero=i;
+            if(S.charAt(i)=='1') one=i;
+            if(S.charAt(i)=='2') two =i;
+
+            if(zero==-1 || one==-1 || two==-1) continue;
+            int min = Math.min(Math.min(one,zero),two);
+            int max = Math.max(Math.max(one,zero),two);
+            res = Math.min(res,max-min+1);
+
+        }
+        return res==100001?-1:res;
+    }
+}
+/*
+Given a string S consisting of the characters 0, 1 and 2. Your task is to find the length of the smallest substring of string S that contains all the three characters 0, 1 and 2. If no such substring exists, then return -1.
+
+Example 1:
+
+Input:
+S = 10212
+Output:
+3
+Explanation:
+The substring 102 is the smallest substring
+that contains the characters 0, 1 and 2.
+Example 2:
+
+Input:
+S = 12121
+Output:
+-1
+Explanation:
+As the character 0 is not present in the
+string S, therefor no substring containing
+all the three characters 0, 1 and 2
+exists. Hence, the answer is -1 in this case.
+Your Task:
+Complete the function smallestSubstring() which takes the string S as input, and returns the length of the smallest substring of string S that contains all the three characters 0, 1 and 2.
+
+Expected Time Complexity: O( length( S ) )
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ length( S ) ≤ 105
+All the characters of String S lies in the set {'0', '1', '2'}
+ */
